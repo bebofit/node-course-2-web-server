9dcf3a965bc2539da19c2906325b28484e315603
bebofit <bebofit@aucegypt.edu>
2019-02-20 20:51:44 +0200

    Setup for Heroku

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
-const fs = require('fs')
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
 var app = express();
 app.set('view engine','hbs')
 
@@ -36,6 +38,6 @@ app.get('/about',(req,res)=>{
         currentYear : new Date().getFullYear()
     });
 })
-app.listen(3000, ()=>{
-    console.log('Server is up on Port 3000');
+app.listen(port, ()=>{
+    console.log(`Server is up on Port ${port}`);
 }); 
